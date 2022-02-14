9b3c9d704715b71cc4b8ee82ada8906e95336a43
unknown <dongwoo.jeong@outlook.com>
2022-02-14 02:43:53 -0500

    add function

--- Function/1.py
+++ Function/1.py
@@ -1,3 +1,8 @@
 def a3():
     print('aaa')
 a3()
+print("---")
+def a3():
+    return 'aaa'
+print(a3())
+print("---")
--- Function/1.rb
+++ Function/1.rb
@@ -2,3 +2,9 @@ def a3()
     puts('aaa')
 end
 a3()
+puts('---')
+def a3()
+    return 'aaa'
+end
+puts(a3())
+puts('---')
